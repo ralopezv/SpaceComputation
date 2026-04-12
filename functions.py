cefd3952783146cebbeb141972d3b69ca85534da
Raul Lopez <ralopezv@yahoo.com>
2026-04-11 22:22:57 -0600

    Added light year conversion function

--- functions.py
+++ functions.py
@@ -1,3 +1,7 @@
 def AUConversion(distanceInKilometers):
     AstronomicalUnit = 1.49598 * 10^8
     return distanceInKilometers/AstronomicalUnit
+
+def LightYearConversion(distanceInKilometers):
+    LightYear = 9.461 * 10^12
+    return distanceInKilometers/LightYear   
